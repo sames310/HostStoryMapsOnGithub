e0da4313beff665a30ac6d5e6d70a2388a15913b
sames310 <38669895+sames310@users.noreply.github.com>
2018-11-19 10:40:33 -0500

    Add files via upload

--- app/custom-scripts.js
+++ app/custom-scripts.js
@@ -8,5 +8,43 @@ define(["dojo/topic"], function(topic) {
     /*
      * Custom Javascript to be executed when the application is ready goes here
      */
+	 $(".selector-background").click(function(){
+				var pos = $(this).position().left;
+				var selection = $(this).index();
+				if($(this).index() > 0){
+					pos = pos + 20;
+					$("#").addClass("active");
+				}
+				if($(this).index() === 0){
+					_fadeLayer = "less_than_15"
+				}
+				else if ($(this).index() === 1){
+					_fadeLayer = "less_than_30"
+				}
+				else if ($(this).index() === 2){
+					_fadeLayer = "less_than_45"
+				}
+				else{
+					_fadeLayer = "greater_than_45"
+				}
+				$("#item-runner").animate({
+					"left": pos
+				},{
+					complete: function(){
+						$(".selector-background").removeClass("active");
+						$(".selector-background:lt(" + selection + ")").addClass("active");
+						$(".selector-background:eq(" + selection + ")").addClass("active");
+					}
+				},100);
+
+				var layers = [];
+				var lyr = getLayerByName(app.maps[0],_fadeLayer,true,false);
+				layers = layers.concat(lyr);
+
+				fadeLayers(layers);
+
+				syncPopup();
+			});
+
   });
 });
